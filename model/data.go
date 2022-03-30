1ed1cf28039a6a261e9d457b1a7ba8b8b6bd3c0a
Siriayanur <meemusiri@gmail.com>
2022-03-30 09:56:56 +0530

    Sort based on user choice

--- model/data.go
+++ model/data.go
@@ -2,15 +2,17 @@ package model
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"os"
-	"reflect"
+	"sort"
 
 	"github.com/Siriayanur/Assignment2/utils"
 )
 
 type Data struct {
-	Students []Student
+	Students     []Student
+	TrackRollNum map[string]bool
 }
 
 func CreateStudentArray() (*Data, error) {
@@ -19,37 +21,103 @@ func CreateStudentArray() (*Data, error) {
 
 	// var data []Student
 	data.Students, err = ReadDataFromDisk()
-	fmt.Print(reflect.ValueOf(data).Kind())
+	// populate TrackRollNum map
+	data.populateMap(data.Students)
+	// fmt.Print(reflect.ValueOf(data).Kind())
 	if err != nil {
 		fmt.Println("no error")
 	}
 	return &data, nil
 }
+func (d *Data) populateMap(students []Student) {
+	for i := 0; i < len(students); i++ {
+		d.TrackRollNum[students[i].RollNumber] = true
+	}
+}
+func (d *Data) AddStudentDetails() error {
 
-func (d *Data) AddStudentDetails() {
 	fullName, age, rollNumber, address := readUserDetails()
 	var coursesEnrolled []Course = readCourseDetails()
 	student := Student{FullName: fullName, Address: address, Age: age, RollNumber: rollNumber, CourseEnrolled: coursesEnrolled}
 	invalidStudent := student.ValidateStudentDetails()
 	if invalidStudent != nil {
-		fmt.Println(invalidStudent.Error())
-		os.Exit(1)
+		return invalidStudent
+	}
+	if d.TrackRollNum[student.RollNumber] {
+		return errors.New("Roll Number already exists")
 	}
 	d.Students = append(d.Students, student)
+	return nil
 }
-
+func getSortParameter() (int, int) {
+	var choice1, choice2 int
+	fmt.Println("Enter the parameter to consider for sorting : ")
+	fmt.Println("1.Full Name 2.Roll Number 3.Age 4.Address : ")
+	fmt.Scanln(&choice1)
+	fmt.Println("Enter the order for sorting 1-asce 2-desc : ")
+	fmt.Scanln(&choice2)
+	return choice1, choice2
+}
 func (d *Data) DisplayStudents() {
-	fmt.Println("Came here")
+	// ask for sorting parameter and order
+	sortParameter, sortOrder := getSortParameter()
+	switch sortParameter {
+	case 1:
+		sort.Slice(d.Students, func(i int, j int) bool {
+			if d.Students[i].FullName == d.Students[j].FullName {
+				return d.Students[i].RollNumber < d.Students[j].RollNumber
+			}
+			return d.Students[i].FullName < d.Students[j].FullName
+		})
+	case 2:
+		sort.Slice(d.Students, func(i int, j int) bool {
+			return d.Students[i].RollNumber < d.Students[j].RollNumber
+		})
+	case 3:
+		sort.Slice(d.Students, func(i int, j int) bool {
+			return d.Students[i].Age < d.Students[j].Age
+		})
+	case 4:
+		sort.Slice(d.Students, func(i int, j int) bool {
+			if d.Students[i].Address == d.Students[j].Address {
+				return d.Students[i].RollNumber < d.Students[j].RollNumber
+			}
+			return d.Students[i].Address < d.Students[j].Address
+		})
+	default:
+		fmt.Println("Invalid Sort Choice")
+		os.Exit(1)
+	}
+	if sortOrder != 1 && sortOrder != 2 {
+		fmt.Println("Invalid Ordering Choice")
+		os.Exit(1)
+	}
+	if sortOrder == 2 {
+		d.Students = reverseArray(d.Students)
+	}
 	for i := 0; i < len(d.Students); i++ {
 		d.Students[i].SingleStudentDetail()
 	}
-	fmt.Println("Came here")
 }
-
+func reverseArray(s []Student) []Student {
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		s[i], s[j] = s[j], s[i]
+	}
+	return s
+}
 func (d *Data) DeleteStudentDetails() {
 
 }
 func (d *Data) SaveStudentDetails() {
+	// sort
+	sort.Slice(d.Students, func(i int, j int) bool {
+		// if fullName same, then sort with rollNum
+		if d.Students[i].FullName == d.Students[i].FullName {
+			return d.Students[i].RollNumber < d.Students[i].RollNumber
+		}
+		return d.Students[i].FullName < d.Students[i].FullName
+	})
+	// save the sorted data to disk
 	err := SaveDataToDisk(d.Students)
 	if err != nil {
 		fmt.Println(err)
@@ -57,6 +125,9 @@ func (d *Data) SaveStudentDetails() {
 	}
 }
 func (d *Data) ConfirmExit() {
+	// Ask if they want to save data
+	d.SaveStudentDetails()
+	// else discard or exit
 }
 func readUserDetails() (string, int, string, string) {
 	var fullName string
